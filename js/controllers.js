639620e018a0b2c091291260c6fd83e21592148f
sergienko <eresiarh@ovi.com>
2014-11-13 05:44:33 +0300

    1

--- js/controllers.js
+++ js/controllers.js
@@ -5,5 +5,10 @@ poemHead.controller('poemHeadCtrl', function ($scope, $http) {
     $http.get('data/poem.json').success(function (arr) {
         $scope.poems = arr;
     });
-$scope.orderProp = 'head';    
+    $scope.orderProp = 'head';
+    $scope.AddHead = function () {
+        $scope.poems.push({head: $scope.heading, snippet: ""});
+        $scope.heading = '';
+        $http.post('data/poem.json', $scope.poems);
+    };
 });
